38983dad0d4df49617a6f93ddfdf129017e2b228
Akuma Isaac Akuma <Akumanewton@gmail.com>
2018-08-31 19:33:42 +0100

    readme and error handling update

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Nedb-Async
+**nedb-async is a simply promise base wrapper methods for <a href="https://github.com/louischatriot/nedb">Nedb</a>**
+
+How to use? Is very simply, so to avoid to much complexity all the cursor modifiers for ``find findOne and count`` methods have to be pass and as the last argument in a array to differentiate is from the
+*query, projection and option* parameters which are mainly `object`
+
+****
+**Very important to know that all the promise base methods starts with async then the method name in camel casing**
+- asyncFind
+- asyncFindOne
+- asyncCount
+- asyncInsert
+- asyncUpdate
+- asyncRemove
+- asyncEnsureIndex
+- asyncRemoveIndex
+
+and every other nedb origin methods are still available
+
+## Usuage:
+
+```js
+  db.asyncFind({},[ ['sort', {name: -1}], ['limit',2] ]).then(function (docs) {
+
+  }).catch(function(error) {
+
+  })
+  //Or with async await
+  async function getUsers() {
+    let users = await db.asyncFind({}, [ ['limit', 100] ])
+  }
+  
+  async function countAllUsers() {
+    let users = await db.asyncCount({})
+  }
+```
+That's pretty very thing for more please use official <a href="https://github.com/louischatriot/nedb">Nedb</a> documentation or create an issue
--- lib/util.ts
+++ lib/util.ts
@@ -1,19 +1,49 @@
 export const promisefy = function (name: string, arg: []) {
   return new Promise((rs, rj) => {
+      /**
+       * Check if the last argument is
+       * an Array.
+       * I choose array because all Nedb
+       * parameters are objec
+       */
       if (Array.isArray(arg[arg.length - 1])) {
-        const modifies = arg.pop(),
+        const modifies = arg.pop()
+        /**
+         * nedb methods like
+         * find, count, and findOne
+         * returns cursor when a 
+         * callback is not passed 
+         * to them. 
+         */
         // @ts-ignore
-          f = this[name](...arg);
-        let c = 0;
-          ++c;
+        const  cursor = this[name](...arg);
+        let count = 1;
         // @ts-ignore
-        for (const fb of modifies) {
-          if (c === arg.length) {
-            f[fb[0]](fb[1]).exec((err:any, docs:any) => {
+        for (const modify of modifies) {
+          let valid = ['sort','limit','skip']
+          if (valid.indexOf(modify[0]) == -1)
+            throw new Error(`${modify[0]} is not recognize, available methods are ${valid.join(',')}`)
+          if(typeof modify[0] !== 'string')
+            throw new Error(`${modify[0]} must be a type of string, available methods are ${valid.join(',')}`)
+          /**
+           * If this modify is the last
+           * element of the modifies
+           * array append exec method
+           * the modify array contains
+           * two elements, first element
+           * being the method name while
+           * the last element the method 
+           * arugument
+           * ['sort',{name: -1, createdAt: -1}]
+           * and so on
+           */
+          if (count === arg.length) {
+            cursor[modify[0]](modify[1]).exec((err:any, docs:any) => {
               if (err) rj(err)
               else rs(docs)
             });
-          } else f[fb[0]](fb[1]);
+          } else cursor[modify[0]](modify[1]);
+          count++
         }
       } else
       // @ts-ignore
