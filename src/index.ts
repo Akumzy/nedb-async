db65b2e6d0277b88771a421a3b505b2bd2861192
jesse <jessedp@gmail.com>
2019-11-13 10:13:29 -0500

    run formatter on src files

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,47 @@
 import Nedb = require('nedb');
-import { justPromise , promisefy} from './util'
+import { justPromise, promisefy } from './util';
 
 export class AsyncNedb<G> extends Nedb<G> {
   constructor(pathOrOptions?: string | Nedb.DataStoreOptions | undefined) {
-    super(pathOrOptions)
+    super(pathOrOptions);
   }
   public asyncFind<T>(query: any, projection?: T) {
-    return promisefy.call(this, 'find', arguments) as Promise<T[]>
+    return promisefy.call(this, 'find', arguments) as Promise<T[]>;
   }
   public asyncCount(query: any) {
-    return promisefy.call(this, 'count', arguments)
+    return promisefy.call(this, 'count', arguments);
   }
 
   public asyncFindOne<T>(query: any, projection?: T) {
-    return promisefy.call(this, 'findOne', arguments) as Promise<T>
+    return promisefy.call(this, 'findOne', arguments) as Promise<T>;
   }
 
   public asyncInsert<T>(newDoc: T) {
-    return justPromise.call(this, 'insert', arguments)
+    return justPromise.call(this, 'insert', arguments);
   }
 
   public asyncUpdate(query: any, updateQuery: any, options?: Nedb.UpdateOptions) {
-    return justPromise.call(this, 'update', arguments)
+    return justPromise.call(this, 'update', arguments);
   }
 
   public asyncRemove(query: any, options?: Nedb.RemoveOptions) {
-    return justPromise.call(this, 'remove', arguments)
+    return justPromise.call(this, 'remove', arguments);
   }
 
   public asyncEnsureIndex(options: Nedb.EnsureIndexOptions) {
-    return justPromise.call(this, 'ensureIndex', arguments)
+    return justPromise.call(this, 'ensureIndex', arguments);
   }
 
   public asyncRemoveIndex(fieldName: string) {
-    return justPromise.call(this, 'removeIndex', arguments)
+    return justPromise.call(this, 'removeIndex', arguments);
   }
 
   public asyncLoadDatabase() {
     return new Promise((resolve, reject) => {
       this.loadDatabase(err => {
-        err ? reject(err) : resolve(true)
-      })
-    })
+        err ? reject(err) : resolve(true);
+      });
+    });
   }
 }
-export default AsyncNedb
+export default AsyncNedb;
--- src/util.ts
+++ src/util.ts
@@ -1,79 +1,79 @@
-export function promisefy<T>(name: string, args:IArguments):Promise<T[]|T> {
+export function promisefy<T>(name: string, args: IArguments): Promise<T[] | T> {
   return new Promise((rs, rj) => {
+    /**
+     * Check if the last argument is
+     * an Array.
+     * I choose array because all Nedb
+     * parameters are objects.
+     */
+    const arg = [...args];
+    if (Array.isArray(arg[arg.length - 1])) {
+      const modifies = arg.pop();
       /**
-       * Check if the last argument is
-       * an Array.
-       * I choose array because all Nedb
-       * parameters are objec
+       * nedb methods like
+       * find, count, and findOne
+       * returns cursor when a
+       * callback is not passed
+       * to them.
        */
-      const arg = [...args];
-      if (Array.isArray(arg[arg.length - 1])) {
-        const modifies = arg.pop();
-        /**
-         * nedb methods like
-         * find, count, and findOne
-         * returns cursor when a 
-         * callback is not passed 
-         * to them. 
-         */
-        // @ts-ignore
-        const  cursor = this[name](...arg);
-        let count = 1;
-        // @ts-ignore
-        for (const modify of modifies) {
-          const valid = ['sort','limit','skip'];
-          if (valid.indexOf(modify[0]) === -1) {
-            throw new Error(`${modify[0]} is not recognize, available methods are ${valid.join(',')}`)
-          }
-          if(typeof modify[0] !== 'string') {
-            throw new Error(`${modify[0]} must be a type of string, available methods are ${valid.join(',')}`)
-          }
-          /**
-           * If this modify is the last
-           * element of the modifies
-           * array append exec method
-           * the modify array contains
-           * two elements, first element
-           * being the method name while
-           * the last element the method 
-           * arugument
-           * ['sort',{name: -1, createdAt: -1}]
-           * and so on
-           */
-          if (count === arg.length) {
-            cursor[modify[0]](modify[1]).exec((err:any, docs:any) => {
-              if (err){
-                rj(err)
-              } else {
-                rs(docs)
-              }
-            });
-          } else {
-            cursor[modify[0]](modify[1]);
-          }
-          count++
+      // @ts-ignore
+      const cursor = this[name](...arg);
+      let count = 1;
+      // @ts-ignore
+      for (const modify of modifies) {
+        const valid = ['sort', 'limit', 'skip'];
+        if (valid.indexOf(modify[0]) === -1) {
+          throw new Error(`${modify[0]} is not recognize, available methods are ${valid.join(',')}`);
         }
-      } else{
-        // @ts-ignore
-        this[name](...arg, (err: any, docs: any) => {
-          if (err){
-            rj(err)
-          } else {
-            rs(docs)
-          }
-        });
+        if (typeof modify[0] !== 'string') {
+          throw new Error(`${modify[0]} must be a type of string, available methods are ${valid.join(',')}`);
+        }
+        /**
+         * If this modify is the last
+         * element of the modifies
+         * array append exec method
+         * the modify array contains
+         * two elements, first element
+         * being the method name while
+         * the last element the method
+         * arugument
+         * ['sort',{name: -1, createdAt: -1}]
+         * and so on
+         */
+        if (count === arg.length) {
+          cursor[modify[0]](modify[1]).exec((err: any, docs: any) => {
+            if (err) {
+              rj(err);
+            } else {
+              rs(docs);
+            }
+          });
+        } else {
+          cursor[modify[0]](modify[1]);
+        }
+        count++;
       }
-    });
+    } else {
+      // @ts-ignore
+      this[name](...arg, (err: any, docs: any) => {
+        if (err) {
+          rj(err);
+        } else {
+          rs(docs);
+        }
+      });
+    }
+  });
 }
-export function justPromise (name: string, arg:IArguments) {
-  return new Promise((rs, rj)=>{
+export function justPromise(name: string, arg: IArguments) {
+  return new Promise((rs, rj) => {
     // @ts-ignore
     this[name](...arg, (err: any, docs: any) => {
       if (err) {
-        rj(err)
+        rj(err);
       } else {
         rs(docs);
       }
-    })
-  })
+    });
+  });
 }
