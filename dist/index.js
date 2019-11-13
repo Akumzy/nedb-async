1e81830c0f7c3b25180be4cb9aa588bb38174d86
jesse <jessedp@gmail.com>
2019-11-13 10:13:01 -0500

    remove the dist dir that shouldn't be in repo

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const util_1 = require("./util");
 const Nedb = require("nedb");
+const util_1 = require("./util");
 class AsyncNedb extends Nedb {
     constructor(pathOrOptions) {
         super(pathOrOptions);
