7b3e3f4ce7240bc36a068bb426d763219e03775a
Akuma Isaac Akuma <akumaisaacakuma@gmail.com>
2019-11-16 09:18:22 +0100

    added few stuffs

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,5 @@
 import AsyncNedb from './index';
 import { join } from 'path';
-import { expect } from 'chai';
 import { unlinkSync } from 'fs';
 
 const dbPath = join(__dirname, 'test.db');
@@ -10,22 +9,21 @@ describe('AsyncNedb unit test', () => {
     age: number;
   }
   let db: AsyncNedb<IUser>;
-  before(done => {
+  beforeAll(() => {
     db = new AsyncNedb({
       filename: dbPath,
       autoload: true,
     });
-    done();
   });
-  it('should inset into database', async () => {
+  test('should inset into database', async () => {
     const user = await db.asyncInsert({ name: 'Akuma Isaac Akuma', age: 200 });
-    expect(user).ownProperty('name', 'Akuma Isaac Akuma');
+    expect(user).toHaveProperty('name', 'Akuma Isaac Akuma');
   });
-  it('Should find a user', async () => {
+  test('Should find a user', async () => {
     const user = await db.asyncFind({ name: 'Akuma Isaac Akuma' });
-    expect(user.length).gte(0);
+    expect(user.length).toBeGreaterThan(0);
   });
-  after(() => {
+  afterAll(() => {
     unlinkSync(dbPath);
   });
 });
