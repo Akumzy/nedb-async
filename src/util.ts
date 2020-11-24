de8b5058aa056f829d1a997dc16cb2b899c41269
Akuma Isaac Akuma <akumaisaacakuma@gmail.com>
2020-11-24 23:57:59 +0100

    few refactoring

--- src/util.ts
+++ src/util.ts
@@ -19,49 +19,26 @@ export function promisefy<T>(name: string, args: IArguments): Promise<T[] | T> {
       // @ts-ignore
       const cursor = this[name](...arg);
       let count = 1;
-      // @ts-ignore
-      for (const modify of modifies) {
+
+      for (const [method, value] of modifies) {
         const valid = ['sort', 'limit', 'skip'];
-        if (valid.indexOf(modify[0]) === -1) {
-          throw new Error(`${modify[0]} is not recognize, available methods are ${valid.join(',')}`);
+        if (valid.includes(method)) {
+          throw new Error(`${method} is not recognized, available methods are ${valid.join(',')}`);
         }
-        if (typeof modify[0] !== 'string') {
-          throw new Error(`${modify[0]} must be a type of string, available methods are ${valid.join(',')}`);
+        if (typeof method !== 'string') {
+          throw new Error(`${method} must be a type of string, available methods are ${valid.join(',')}`);
         }
-        /**
-         * If this modify is the last
-         * element of the modifies
-         * array append exec method
-         * the modify array contains
-         * two elements, first element
-         * being the method name while
-         * the last element the method
-         * argument
-         * ['sort',{name: -1, createdAt: -1}]
-         * and so on
-         */
+        // call exec method if it's the last
         if (count === arg.length) {
-          cursor[modify[0]](modify[1]).exec((err: any, docs: any) => {
-            if (err) {
-              rj(err);
-            } else {
-              rs(docs);
-            }
-          });
+          cursor[method](value).exec((err: any, docs: any) => (err ? rj(err) : rs(docs)));
         } else {
-          cursor[modify[0]](modify[1]);
+          cursor[method](value);
         }
         count++;
       }
     } else {
       // @ts-ignore
-      this[name](...arg, (err: any, docs: any) => {
-        if (err) {
-          rj(err);
-        } else {
-          rs(docs);
-        }
-      });
+      this[name](...arg, (err: any, docs: any) => (err ? rj(err) : rs(docs)));
     }
   });
 }
